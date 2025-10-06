91a2390ff2452cbe3eff59e4331ed04ad090218e
Alejandro Bores Rivera <boresrivera@gmail.com>
2025-10-06 15:04:42 -0500

    Update save_image.js

--- analyzers/save_image.js
+++ analyzers/save_image.js
@@ -2,9 +2,9 @@ const path = require('path');
 const fs = require('fs');
 
 const fetch = require('node-fetch');
-const { FormData } = require('form-data');
-const { FormDataEncoder } = require('form-data-encoder');
-const { Readable } = require('stream');
+const FormData = require('form-data');
+const FormDataEncoder = require('form-data-encoder');
+const Readable = require('stream');
 
 async function saveImage(imageData) {
 
